6fd909a3975e1a22322c18958bd57cc434656068
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2023-05-03 21:51:10 +0800

    Update diy-part2.sh

--- padavanonly/Redmi-AX6000/diy-part2.sh
+++ padavanonly/Redmi-AX6000/diy-part2.sh
@@ -20,3 +20,33 @@ sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_genera
 #mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
 #rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##---------------------------------------------------------
+# Replace with passwall
+rm openwrt/feeds/luci/applications/luci-app-passwall -rf
+# Replace with ssr-plus
+rm openwrt/feeds/luci/applications/luci-app-ssr-plus -rf
+rm openwrt/feeds/packages/net/brook -rf
+rm openwrt/feeds/packages/net/chinadns-ng -rf
+rm openwrt/feeds/packages/net/dns2socks -rf
+rm openwrt/feeds/packages/net/dns2tcp -rf
+rm openwrt/feeds/packages/net/hysteria -rf
+rm openwrt/feeds/packages/net/ipt2socks -rf
+rm openwrt/feeds/packages/net/microsocks -rf
+rm openwrt/feeds/packages/net/naiveproxy -rf
+rm openwrt/feeds/packages/net/pdnsd-alt -rf
+rm openwrt/feeds/packages/net/redsocks2 -rf
+rm openwrt/feeds/packages/net/shadowsocksr-libev -rf
+rm openwrt/feeds/packages/net/shadowsocks-rust -rf
+rm openwrt/feeds/packages/net/simple-obfs -rf
+rm openwrt/feeds/packages/net/sing-box -rf
+rm openwrt/feeds/packages/net/ssocks -rf
+rm openwrt/feeds/packages/net/tcping -rf
+rm openwrt/feeds/packages/net/trojan -rf
+rm openwrt/feeds/packages/net/trojan-go -rf
+rm openwrt/feeds/packages/net/trojan-plus -rf
+rm openwrt/feeds/packages/net/v2raya -rf
+rm openwrt/feeds/packages/net/v2ray-core -rf
+rm openwrt/feeds/packages/net/v2ray-geodata -rf
+rm openwrt/feeds/packages/net/v2ray-plugin -rf
+rm openwrt/feeds/packages/net/xray-core -rf
+rm openwrt/feeds/packages/net/xray-plugin -rf
+#rm openwrt/package/lean/luci-theme-argon -rf
