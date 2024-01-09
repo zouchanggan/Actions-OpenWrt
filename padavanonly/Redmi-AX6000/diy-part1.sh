4b597186ab84a2c8007635bc6d58ae4bd9bdddb5
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-01-09 10:27:24 +0800

    Update diy-part1.sh

--- padavanonly/Redmi-AX6000/diy-part1.sh
+++ padavanonly/Redmi-AX6000/diy-part1.sh
@@ -13,29 +13,12 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 删除软件包
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/luci/applications/luci-app-vssr 
-# 删除软件依赖
-rm -rf feeds/packages/net/brook 
-rm -rf feeds/packages/net/chinadns-ng 
-rm -rf feeds/packages/net/hysteria 
-rm -rf feeds/packages/net/shadowsocksr-libev
-rm -rf feeds/packages/net/shadowsocks-rust 
-rm -rf feeds/packages/net/tcping 
-rm -rf eeds/packages/net/trojan 
-rm -rf feeds/packages/net/trojan-go
-rm -rf feeds/packages/net/trojan-plus 
-rm -rf feeds/packages/net/v2raya
-rm -rf feeds/packages/net/v2ray-core 
-rm -rf feeds/packages/net/v2ray-geodata 
-rm -rf feeds/packages/net/v2ray-plugin
-rm -rf feeds/packages/net/xray-core 
-rm -rf feeds/packages/net/xray-plugin
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/lang/golang
+#添加额外软件包
+git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+git clone https://github.com/kenzok8/small.git package/small
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Add a feed source
 #echo 'src-git helloworld https://githubfast.com/fw876/helloworld' >>feeds.conf.default
