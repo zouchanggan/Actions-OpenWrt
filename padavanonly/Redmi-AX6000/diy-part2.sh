a1c9f31676dff1b4ff35cf7ee137a02496cb3d9e
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-01-14 14:18:07 +0800

    Update diy-part2.sh

--- padavanonly/Redmi-AX6000/diy-part2.sh
+++ padavanonly/Redmi-AX6000/diy-part2.sh
@@ -13,28 +13,26 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 
-# 删除软件包
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-# rm -rf feeds/luci/applications/luci-app-vssr 
-
 # 删除软件依赖
-rm -rf feeds/packages/net/trojan
-rm -rf feeds/packages/net/trojan-go
-rm -rf feeds/packages/net/trojan-plus 
+
 rm -rf feeds/packages/net/chinadns-ng 
 rm -rf feeds/packages/net/hysteria 
+rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/shadowsocksr-libev
 rm -rf feeds/packages/net/shadowsocks-rust 
 rm -rf feeds/packages/net/tcping 
+rm -rf feeds/packages/net/trojan
+rm -rf feeds/packages/net/trojan-go
+rm -rf feeds/packages/net/trojan-plus
+rm -rf feeds/packages/net/tuic-client  
 rm -rf feeds/packages/net/v2raya
 rm -rf feeds/packages/net/v2ray-core  
 rm -rf feeds/packages/net/v2ray-plugin
 rm -rf feeds/packages/net/xray-core 
 rm -rf feeds/packages/net/xray-plugin
-rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/lang/golang
+rm -rf package/libs/mbedtls
 
 #添加额外软件包
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
@@ -42,9 +40,17 @@ git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/zouchanggan/mbedtls.git package/libs/mbedtls
 
 # 替换软件依赖
 cp -r package/helloworld/mosdns feeds/packages/net
 cp -r package/small/trojan feeds/packages/net
 cp -r package/small/trojan-go feeds/packages/net
 cp -r package/small/trojan-plus feeds/packages/net
+cp -r package/small/tuic-client feeds/packages/net
+
+# 删除软件包
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf package/helloworld/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-vssr
