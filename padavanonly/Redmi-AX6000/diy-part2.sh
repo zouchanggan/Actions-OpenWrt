2bea3d20c3a64506e4e6645bcbb2a7751a7edfd1
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-02-18 16:26:29 +0800

    Update diy-part2.sh

--- padavanonly/Redmi-AX6000/diy-part2.sh
+++ padavanonly/Redmi-AX6000/diy-part2.sh
@@ -14,7 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 
 # 删除软件依赖
-
 rm -rf feeds/packages/net/chinadns-ng 
 rm -rf feeds/packages/net/hysteria 
 rm -rf feeds/packages/net/mosdns
@@ -34,32 +33,37 @@ rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/open-app-filter
 rm -rf feeds/packages/lang/golang
 rm -rf package/libs/mbedtls
-
+ 
 #添加额外软件包
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 git clone https://github.com/kenzok8/small.git package/small
-git clone https://github.com/zouchanggan/helloworld.git package/helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall
+git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/zouchanggan/mbedtls.git package/libs/mbedtls
 git clone https://github.com/zouchanggan/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/zouchanggan/SSRP.git package/SSRP
+
+# 删除软件包
+rm -rf package/helloworld/luci-app-ssr-plus
+rm -rf package/helloworld/shadowsocks-rust
+rm -rf package/helloworld/shadowsocksr-libev
+rm -rf package/helloworld/v2raya
 
 # 替换软件依赖
-rm -rf package/small/v2raya
-rm -rf package/small/shadow-tls
-rm -rf package/small/shadowsocks-rust
-cp -r package/helloworld/shadowsocks-rust package/small
-cp -r package/helloworld/v2raya package/small
-cp -r package/helloworld/shadow-tls package/small
-cp -r package/helloworld/mosdns feeds/packages/net
-cp -r package/small/trojan feeds/packages/net
-cp -r package/small/trojan-go feeds/packages/net
-cp -r package/small/trojan-plus feeds/packages/net
-cp -r package/small/tuic-client feeds/packages/net
+cp -r package/small/luci-app-ssr-plus package/helloworld
+cp -r package/SSRP/shadowsocks-rust package/helloworld
+cp -r package/SSRP/shadowsocksr-libev package/helloworld
+cp -r package/small/luci-app-passwall package/passwall
+cp -r package/small/luci-app-passwall2 package/passwall
+cp -r package/SSRP/v2raya package/helloworld
+cp -r package/mosdns/mosdns feeds/packages/net
 
 # 删除软件包
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-vssr
 rm -rf feeds/luci/applications/luci-app-appfilter
-rm -rf package/helloworld
+rm -rf package/small
+rm -rf package/SSRP
