de04d1230d28b58d8bc32c708eea1cbedfcf4feb
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-02-20 18:55:41 +0800

    Update diy-part2.sh

--- padavanonly/Redmi-AX6000/diy-part2.sh
+++ padavanonly/Redmi-AX6000/diy-part2.sh
@@ -38,7 +38,8 @@ rm -rf package/libs/mbedtls
 
 #添加额外软件包
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/small
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall
+git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
@@ -48,13 +49,12 @@ git clone https://github.com/zouchanggan/SSRP.git package/SSRP
 
 # 替换软件依赖
 cp -r package/SSRP/v2raya package/small
-cp -r package/helloworld/shadow-tls package/small
-cp -r package/SSRP/shadowsocks-rust package/small
+cp -r package/helloworld/shadow-tls package/passwall
+cp -r package/SSRP/shadowsocks-rust package/passwall
 cp -r package/mosdns/mosdns feeds/packages/net
-cp -r package/small/trojan feeds/packages/net
-cp -r package/small/trojan-go feeds/packages/net
-cp -r package/small/trojan-plus feeds/packages/net
-cp -r package/small/tuic-client feeds/packages/net
+cp -r package/small/luci-app-passwall package/passwall
+cp -r package/small/luci-app-passwall2 package/passwall
+cp -r package/small/luci-app-ssr-plus package/passwall
 
 # 删除软件包
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -62,5 +62,6 @@ rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-vssr
 rm -rf feeds/luci/applications/luci-app-appfilter
 rm -rf package/helloworld
+rm -rf package/small
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
