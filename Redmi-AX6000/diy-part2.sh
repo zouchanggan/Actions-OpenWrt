67cf4dfeae0e9e118026916d533866112de87614
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-05-26 12:58:24 +0800

    Update diy-part2.sh

--- Redmi-AX6000/diy-part2.sh
+++ Redmi-AX6000/diy-part2.sh
@@ -19,6 +19,9 @@ rm -rf feeds/packages/net/open-app-filter
 rm -rf feeds/packages/lang/golang
 rm -rf package/libs/mbedtls
 rm -rf feeds/packages/net/ddns-go
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/luci/applications/luci-app-mosdns
  
 #添加额外软件包
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
@@ -28,36 +31,17 @@ git clone https://github.com/zouchanggan/OpenAppFilter.git package/OpenAppFilter
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
 git clone https://github.com/zouchanggan/SSRP.git package/SSRP
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+
 
 # 删除软件包
-rm -rf package/passwall-packages/shadowsocks-rust
-rm -rf package/passwall-packages/shadowsocksr-libev
-rm -rf package/helloworld/chinadns-ng
-rm -rf package/helloworld/mosdns
-# rm -rf package/helloworld/shadowsocksr-libev
-rm -rf package/helloworld/shadowsocks-rust
-rm -rf package/helloworld/v2raya
+rm -rf package/small/shadowsocks-rust
+rm -rf package/small/shadowsocksr-libev
+rm -rf package/small/v2raya
 
 # 替换软件&依赖
-# cp -rf package/SSRP/update/shadowsocks-rust package/small
-# cp -rf package/SSRP/update/shadowsocksr-libev package/small
-# cp -rf package/SSRP/update/v2raya package/small
-cp -rf package/SSRP/update/shadowsocks-rust package/passwall-packages
-cp -rf package/SSRP/update/shadowsocksr-libev package/passwall-packages
-# cp -rf package/SSRP/update/shadowsocksr-libev package/helloworld
-cp -rf package/SSRP/update/shadowsocks-rust package/helloworld
-cp -rf package/SSRP/update/v2raya package/helloworld
-cp -rf package/passwall/luci-app-passwall package/passwall-packages
-cp -rf package/passwall2/luci-app-passwall2 package/passwall-packages
-cp -rf package/small/luci-app-mosdns package/passwall-packages
-cp -rf package/small/mosdns package/passwall-packages
-cp -rf package/small/v2dat package/passwall-packages
-cp -rf package/small/chinadns-ng package/helloworld
-cp -rf package/small/mosdns package/helloworld
+cp -rf package/SSRP/update/shadowsocks-rust package/small
+cp -rf package/SSRP/update/shadowsocksr-libev package/small
+cp -rf package/SSRP/update/v2raya package/small
 
 # 删除软件包
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -66,8 +50,5 @@ rm -rf feeds/luci/applications/luci-app-vssr
 rm -rf feeds/luci/applications/luci-app-appfilter
 rm -rf feeds/luci/applications/luci-app-ddns-go
 rm -rf package/SSRP
-rm -rf package/small
-rm -rf package/passwall
-rm -rf package/passwall2
 # rm -rf package/istore/quickstart
 # rm -rf package/istore/luci-app-quickstart
