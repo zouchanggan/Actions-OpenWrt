e060266b99585a122c1db85d845f32cfdadf0c18
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-12-18 21:57:25 +0800

    Update diy-part1.sh

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
 # Add a feed source
-#echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;lede' >>feeds.conf.default
-git clone -b small https://github.com/zouchanggan/openwrt-packages ./package/small
+# echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;lede' >>feeds.conf.default
+# git clone -b small https://github.com/zouchanggan/openwrt-packages ./package/small
+# sed -i '5i src-git small https://github.com/zouchanggan/openwrt-packages;small' feeds.conf.default
