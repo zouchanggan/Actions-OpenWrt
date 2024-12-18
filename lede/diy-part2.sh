e8e0af049b64969e4c63a0b5a9b0d90090322c03
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-12-18 08:48:33 +0800

    Update diy-part2.sh

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -2,3 +2,39 @@
 #
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 删除软件依赖
+rm -rf feeds/packages/net/chinadns-ng 
+rm -rf feeds/packages/net/hysteria 
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/shadowsocks-rust 
+rm -rf feeds/packages/net/tcping 
+rm -rf feeds/packages/net/trojan
+rm -rf feeds/packages/net/trojan-go
+rm -rf feeds/packages/net/trojan-plus
+rm -rf feeds/packages/net/tuic-client  
+rm -rf feeds/packages/net/v2raya
+rm -rf feeds/packages/net/v2ray-core  
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/xray-core 
+rm -rf feeds/packages/net/xray-plugin
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/open-app-filter
+rm -rf feeds/packages/net/gn
+rm -rf feeds/packages/lang/golang
+rm -rf package/libs/mbedtls
+rm -rf feeds/packages/net/ddns-go
+rm -rf feeds/packages/net/lucky
+
+#添加额外软件包
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# 删除软件包
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-vssr
+rm -rf feeds/luci/applications/luci-app-ddns-go
+rm -rf feeds/luci/applications/luci-app-appfilter
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-lucky
