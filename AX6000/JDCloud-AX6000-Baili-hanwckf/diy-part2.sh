f30fae73301a5dd81f5636397d47e50db079e31e
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2025-01-05 20:30:33 +0800

    Update diy-part2.sh

--- AX6000/JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
+++ AX6000/JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
@@ -58,13 +58,16 @@ rm -rf feeds/luci/applications/luci-app-appfilter
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-lucky
 rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf packages/small/luci-app-homeproxy
+rm -rf packages/small/luci-app-mihomo
+rm -rf packages/small/mihomo
 
 ##-----------------Add OpenClash dev core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+mkdir -p packages/small/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash packages/small/root/etc/openclash/core/clash >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##-----------------Delete DDNS's examples-----------------
 sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
