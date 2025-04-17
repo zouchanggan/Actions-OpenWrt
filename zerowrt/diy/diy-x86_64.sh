73948558c8e78fe988ec98e94195aa0d5c4331c9
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2025-04-18 00:10:22 +0800

    Update diy-x86_64.sh

--- zerowrt/diy/diy-x86_64.sh
+++ zerowrt/diy/diy-x86_64.sh
@@ -60,7 +60,7 @@ sed -i '\#export ENV=/etc/shinit#a export HISTCONTROL=ignoredups' package/base-f
 # sed -i "s/192.168.1.1/192.168.1.10/g" package/base-files/files/bin/config_generate
 
 # 修改名称
-# sed -i 's/OpenWrt/LEDE/' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/immortalwrt/' package/base-files/files/bin/config_generate
 
 # default-settings
 git clone --depth=1 -b openwrt-24.10 https://github.com/oppen321/default-settings package/default-settings
@@ -194,8 +194,8 @@ curl -L https://git.kejizero.online/zhao/files/raw/branch/main/images/bg1.jpg -o
 sed -i "s|_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || '')|_('Firmware Version'), 'ZeroWrt-VIP-R25.04.6-K6.6.83'|" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
 # istoreos
-# sed -i 's/iStoreOS/ZeroWrt/' package/openwrt-package/istoreos-files/files/etc/board.d/10_system
-# sed -i 's/192.168.100.1/192.168.1.10/' package/openwrt-package/istoreos-files/Makefile
+sed -i 's/iStoreOS/OpenWrt/' package/openwrt-package/istoreos-files/files/etc/board.d/10_system
+sed -i 's/192.168.100.1/10.0.0.1/' package/openwrt-package/istoreos-files/Makefile
 
 # update feeds
 ./scripts/feeds update -a
